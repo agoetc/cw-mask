aca19aae3bb1a676fe71c1ca07dc06e26a86cb59
agoetc <tkgw666private@gmail.com>
2022-07-30 19:41:26 +0900

    prettierの適用範囲を広げた

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,32 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
     },
-    'settings': {
-        'react': {
-            'version': 'detect'
-        }
-    }
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 }
