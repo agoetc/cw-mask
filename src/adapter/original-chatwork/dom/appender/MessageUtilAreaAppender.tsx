175923d3e783445cc09e5fed2a66755caef1ea8b
agoetc <tkgw666private@gmail.com>
2022-07-31 09:39:17 +0000

    [ci] format

--- src/adapter/original-chatwork/dom/appender/MessageUtilAreaAppender.tsx
+++ src/adapter/original-chatwork/dom/appender/MessageUtilAreaAppender.tsx
@@ -1,11 +1,9 @@
 import { MessageUtilArea } from '../component/MessageUtilArea'
 
 export const MessageUtilAreaAppender = {
-
     appendMuskButton(messageUtilArea: MessageUtilArea) {
         const muskButton = document.createElement('button')
         muskButton.textContent = 'マスク'
         messageUtilArea.value.appendChild(muskButton)
-    }
-
+    },
 }
