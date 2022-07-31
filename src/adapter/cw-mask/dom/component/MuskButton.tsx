8773558cc0e3e075e82bc4fa5e4dba1a801be9d7
agoetc <tkgw666private@gmail.com>
2022-07-31 18:44:17 +0900

    ボタン表示の処理をReact化

---
+++ src/adapter/cw-mask/dom/component/MuskButton.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+const MuskButton = () => {
+    return <button>マスクボタン</button>
+}
+
+
+export default MuskButton
---
+++ src/adapter/cw-mask/dom/component/MuskForm.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import MuskButton from './MuskButton'
+
+const MuskForm = () => {
+    return (
+        <div>
+            <MuskButton />
+        </div>
+    )
+}
+
+export default MuskForm
