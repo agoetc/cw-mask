97663a54facebd5089d823c01a47754d7e66fc92
agoetc <tkgw666private@gmail.com>
2022-08-06 16:03:16 +0900

    rename and refactor

--- src/adapter/cw-mask/dom/initializer/CwMuskInitializer.tsx
+++ src/adapter/cw-mask/dom/initializer/CwMuskInitializer.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, Root } from 'react-dom/client'
 import { MessageUtilArea } from '../../../original-chatwork/dom/component/MessageUtilArea'
 import { MessageUtilAreaReader } from '../../../original-chatwork/dom/reader/MessageUtilAreaReader'
 import MuskForm from '../component/MuskForm'
 
 const ROOT_ID = 'musk-button-root'
 
-export const CwMuskRender = () => {
+const buildRootElement = () => {
     const rootElement = document.createElement('div')
     rootElement.setAttribute('id', ROOT_ID)
 
-    const messageUtilArea: MessageUtilArea = MessageUtilAreaReader.read()
-    messageUtilArea.prepend(rootElement)
-    const messageUtilAreaRoot = createRoot(rootElement)
+    return rootElement
+}
 
+export const CwMuskInitializer = () => {
+    const messageUtilArea: MessageUtilArea = MessageUtilAreaReader.read()
+
+    const rootElement:HTMLDivElement = buildRootElement()
+    const messageUtilAreaRoot: Root = createRoot(rootElement)
+
+    // original-chatworkにRootElementを追加
+    messageUtilArea.prepend(rootElement)
+
+    // RootElementをcw-muskで置き換え
     messageUtilAreaRoot.render(<MuskForm />)
 }
