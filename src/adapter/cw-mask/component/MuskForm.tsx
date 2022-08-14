66593ca8f804b384fee57fd885909d789e8f1d4b
ame1252 <kyou611yu22@gmail.com>
2022-08-14 15:16:00 +0900

    WIP

--- src/adapter/cw-mask/component/MuskForm.tsx
+++ src/adapter/cw-mask/component/MuskForm.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import { DOMUtil } from '../../../util/dom/DOMUtil'
 import { MessageReader } from '../../original-chatwork/reader/MessageReader'
+import { MessageListModel } from '../model/MessageListModel'
 import MuskButton from './MuskButton'
 
 const MuskForm = () => {
     const musk = () => {
         console.log('test')
         const messageList: HTMLCollectionOf<Element> = MessageReader.getMessageList()
-        const speakerName: HTMLCollectionOf<Element> =
-        DOMUtil.checkNull(messageList.item(1)).getElementsByClassName('_speakerName')
-        const hoge = DOMUtil.checkNull(speakerName.item(0)) as HTMLElement
-        hoge.innerText = 'aiueo'
+        const messageListModel = MessageListModel(messageList)
+
     }
     return (
         <div>
