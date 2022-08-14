6c1141ebfacc9df9be270304155cf2853b4bf7e1
ame1252 <kyou611yu22@gmail.com>
2022-08-14 14:27:36 +0900

    マスクボタンが押されたときに名前が変更される処理を書いた

--- src/adapter/cw-mask/component/MuskForm.tsx
+++ src/adapter/cw-mask/component/MuskForm.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react'
+import { MessageReader } from '../../original-chatwork/reader/MessageReader'
 import MuskButton from './MuskButton'
 
 const MuskForm = () => {
+    const musk = () => {
+        console.log('test')
+        const messageList:  HTMLCollectionOf<Element>= MessageReader.getMessageList()
+        const speakerName: HTMLCollectionOf<Element>= messageList[1].getElementsByClassName('_speakerName')
+        const hoge = speakerName[0] as HTMLElement
+        hoge.innerText = 'aiueo'
+    }
     return (
         <div>
-            <MuskButton onClick={() => console.log('test')} />
+            <MuskButton onClick={musk} />
         </div>
     )
 }
