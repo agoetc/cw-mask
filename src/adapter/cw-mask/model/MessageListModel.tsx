d5399bc95ce857f1da05ad1c0186857ffd1f91c5
agoetc <tkgw666private@gmail.com>
2022-08-20 05:47:23 +0000

    [ci] format

--- src/adapter/cw-mask/model/MessageListModel.tsx
+++ src/adapter/cw-mask/model/MessageListModel.tsx
@@ -2,9 +2,11 @@ import { DOMUtil } from '../../../util/dom/DOMUtil'
 
 export const MessageListModel = (messageList: HTMLCollectionOf<Element>) => ({
     getSpeakerName: () => {
-        Array.prototype.forEach.call( messageList , message  => {
-            const speakerName = DOMUtil.checkNull(message.getElementsByClassName('_speakerName').item(0)) as HTMLElement
+        Array.prototype.forEach.call(messageList, (message) => {
+            const speakerName = DOMUtil.checkNull(
+                message.getElementsByClassName('_speakerName').item(0)
+            ) as HTMLElement
             speakerName.innerText = 'aiueo'
-        });
+        })
     },
 })
