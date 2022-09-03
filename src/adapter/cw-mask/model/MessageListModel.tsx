7bfee6db3a56b80a82302e7db807398db0c84589
ame1252 <kyou611yu22@gmail.com>
2022-09-03 16:04:51 +0900

    リファクタリング

--- src/adapter/cw-mask/model/MessageListModel.tsx
+++ src/adapter/cw-mask/model/MessageListModel.tsx
@@ -2,37 +2,47 @@ import { DOMUtil } from '../../../util/dom/DOMUtil'
 
 export class MessageListModel {
     private messageList: HTMLCollectionOf<Element>
+    private namePairList: NamePair[] = []
+    private num = 1
 
     constructor(messageList: HTMLCollectionOf<Element>) {
         this.messageList = messageList
     }
 
     setSpeakerName = () => {
-        const namePairList: NamePair[] = []
-        let num = 1
         Array.prototype.forEach.call(this.messageList, (message) => {
             const speakerName = this.getSpeakerName(message)
-            const namePair = namePairList.find((namePair) => {
-                return namePair.originalName == speakerName.innerText
-            })
-            if (namePair === undefined) {
-                namePairList.push({
-                    originalName: speakerName.innerText,
-                    maskedName: num.toString(),
-                })
-                speakerName.innerText = num.toString()
-                num++
-            } else {
-                speakerName.innerText = namePair.maskedName
-            }
+            speakerName.innerText = this.buildMaskName(speakerName)
+            
         })
     }
+
     private getSpeakerName = (message: HTMLElement): HTMLElement => {
         return DOMUtil.checkNull(
             message.getElementsByClassName('_speakerName').item(0)
         ) as HTMLElement
     }
+
+    private buildMaskName = (speakerName : HTMLElement): string => {
+        const namePair = this.namePairList.find((namePair) => {
+            return namePair.originalName == speakerName.innerText
+        })
+        let maskName: string 
+        if (namePair === undefined) {
+            this.namePairList.push({
+                originalName: speakerName.innerText,
+                maskedName: this.num.toString(),
+            })
+            maskName = this.num.toString()
+            this.num++
+            
+        } else {
+            maskName = namePair.maskedName
+        }
+        return maskName
+    }
 }
+
 type NamePair = {
     originalName: string
     maskedName: string
