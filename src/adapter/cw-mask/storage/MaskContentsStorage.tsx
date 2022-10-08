8d836ad2cdf77f644aa744ae66c49d0f7ee0293f
ame1252 <107744566+ame1252@users.noreply.github.com>
2022-10-08 13:27:42 +0900

    アイコンを置き換える処理の実装 (#21)
    * アイコンを置き換える処理を実装した

--- src/adapter/cw-mask/storage/MaskContentsStorage.tsx
+++ src/adapter/cw-mask/storage/MaskContentsStorage.tsx
@@ -1,8 +1,18 @@
 export const maskContentsStorage: MaskContents[] = [
-    { name: 'きりん' },
-    { name: 'らいおん' },
-    { name: 'いぬ' },
+    {
+        name: 'きりん',
+        iconPath: 'https://lh3.google.com/u/0/d/1ojuxchgILSgv86JMtc3qBGqSIXxNLahh=w1578-h1848-iv2',
+    },
+    {
+        name: 'らいおん',
+        iconPath: 'https://lh3.google.com/u/0/d/1KV0jgBkE6649yV-ykBoDLvNQXAHUVEUJ=w1578-h1848-iv2',
+    },
+    {
+        name: 'いぬ',
+        iconPath: 'https://lh3.google.com/u/0/d/1EmJ2TlgezQE6TSP0S8WdDsK99PMBqHBK=w3840-h1848-iv2',
+    },
 ]
 export type MaskContents = {
     name: string
+    iconPath: string
 }
