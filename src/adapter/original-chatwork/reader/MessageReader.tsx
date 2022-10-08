3e0d708987d4f1ac0f1f3dbc9479a383effd3e0b
ame1252 <kyou611yu22@gmail.com>
2022-10-08 14:37:30 +0900

    リファクタリング

--- src/adapter/original-chatwork/reader/MessageReader.tsx
+++ src/adapter/original-chatwork/reader/MessageReader.tsx
@@ -1,8 +1,8 @@
 export const MessageReader = {
     // TODO: ユビキタス言語を選定する
-    getMessageList(): Array<Element> {
+    getMessageList(): Array<HTMLElement> {
         const messageList = document.getElementsByClassName('_message')
-        return Array.prototype.slice.call(messageList).filter((message: Element) => {
+        return Array.prototype.slice.call(messageList).filter((message: HTMLElement) => {
             return message.getElementsByClassName('_speaker').length != 0
         })
     },
