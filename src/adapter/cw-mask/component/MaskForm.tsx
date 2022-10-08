685e5e03a1c8cf393877f3a43cc6c3998ea09fcf
ame1252 <kyou611yu22@gmail.com>
2022-10-08 15:17:25 +0900

    ボタンの切り替えをできるようにした

--- src/adapter/cw-mask/component/MaskForm.tsx
+++ src/adapter/cw-mask/component/MaskForm.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react'
+import { useState } from 'react'
 import { MessageReader } from '../../original-chatwork/reader/MessageReader'
 import { MessageListModel } from '../model/MessageListModel'
 import MaskButton from './MaskButton'
+import UnMaskButton from './UnMaskButton'
 
 const MaskForm = () => {
+    const [isMask, setIsMask] = useState(false)
     const mask = () => {
+        setIsMask(!isMask)
         const messageList = MessageReader.getMessageList()
         const messageListModel = new MessageListModel(messageList)
         messageListModel.setContents()
     }
-    return (
-        <div>
-            <MaskButton onClick={mask} />
-        </div>
-    )
+    if (isMask) {
+        return (
+            <div>
+                <UnMaskButton onClick={mask} />
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <MaskButton onClick={mask} />
+            </div>
+        )
+    }
 }
 
 export default MaskForm
