45b646c8547664de702aa53d521e3c6903ff7799
ame1252 <kyou611yu22@gmail.com>
2022-11-13 14:24:07 +0900

    MessageUtilAreaの取得方法を変更した

--- src/adapter/original-chatwork/reader/MessageUtilAreaReader.tsx
+++ src/adapter/original-chatwork/reader/MessageUtilAreaReader.tsx
@@ -1,14 +1,14 @@
 import { DOMUtil } from '../../../util/dom/DOMUtil'
 
-const MESSAGE_UTIL_AREA_CLASS_NAME = 'sc-ilhmMj ckKvyz'
+
 
 export const MessageUtilAreaReader = {
     /**
      * @see https://www.notion.so/a672b2292c5e4db5811e05bae1e38fe1
      */
-    read(): Element {
-        return DOMUtil.checkNull(
-            document.getElementsByClassName(MESSAGE_UTIL_AREA_CLASS_NAME).item(0)
-        )
+    read(): HTMLDivElement {
+        const messageInputArea =document.querySelector('[data-testid="message-input-area"]')
+        return DOMUtil.checkNull(messageInputArea?.children.item(0)?.children.item(1)) as HTMLDivElement
+        
     },
 }
--- src/util/dom/DOMUtil.tsx
+++ src/util/dom/DOMUtil.tsx
@@ -1,6 +1,6 @@
 export const DOMUtil = {
-    checkNull<T extends Element>(element: T | null): T {
-        if (element === null) {
+    checkNull<T extends Node>(element: T | null | undefined): T {
+        if (element === null || element === undefined) {
             throw Error('[error] DOMUtil: can not read element')
         } else {
             console.log('[success] DOMUtil: readed element')
