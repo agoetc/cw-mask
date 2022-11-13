8e6c6a0ebd64faebf4b2bd9232afa9908fbc39cc
agoetc <tkgw666private@gmail.com>
2022-11-13 05:24:32 +0000

    [ci] format

--- src/adapter/original-chatwork/reader/MessageUtilAreaReader.tsx
+++ src/adapter/original-chatwork/reader/MessageUtilAreaReader.tsx
@@ -1,14 +1,13 @@
 import { DOMUtil } from '../../../util/dom/DOMUtil'
 
-
-
 export const MessageUtilAreaReader = {
     /**
      * @see https://www.notion.so/a672b2292c5e4db5811e05bae1e38fe1
      */
     read(): HTMLDivElement {
-        const messageInputArea =document.querySelector('[data-testid="message-input-area"]')
-        return DOMUtil.checkNull(messageInputArea?.children.item(0)?.children.item(1)) as HTMLDivElement
-        
+        const messageInputArea = document.querySelector('[data-testid="message-input-area"]')
+        return DOMUtil.checkNull(
+            messageInputArea?.children.item(0)?.children.item(1)
+        ) as HTMLDivElement
     },
 }
