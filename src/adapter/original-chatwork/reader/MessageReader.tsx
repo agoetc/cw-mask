3ff9231b6f282a351a93f00855794e9d78214668
ame1252 <kyou611yu22@gmail.com>
2022-11-26 13:18:20 +0900

    companyNameの取得方法変更

--- src/adapter/original-chatwork/reader/MessageReader.tsx
+++ src/adapter/original-chatwork/reader/MessageReader.tsx
@@ -3,7 +3,7 @@ import { DOMUtil } from '../../../util/dom/DOMUtil'
 export type MessageElement = HTMLElement
 export type MessageSpeakerNameElement = HTMLAnchorElement
 export type MessageSpeakerIconElement = HTMLImageElement
-export type MessageSpeakerCompanyNameElement = Element
+export type MessageSpeakerCompanyNameElement = HTMLParagraphElement
 
 export const MessageReader = {
     getMessageList(): MessageElement[] {
@@ -21,6 +21,8 @@ export const MessageReader = {
         return DOMUtil.checkNull(message.getElementsByTagName('img').item(0))
     },
     getSpeakerCompanyName(message: MessageElement): MessageSpeakerCompanyNameElement {
-        return DOMUtil.checkNull(message.getElementsByClassName('sc-hrXEzC frFtxh').item(0))
+        return DOMUtil.checkNull(
+            message?.children.item(0)?.children.item(1)?.children.item(0)?.children.item(1)
+        ) as HTMLParagraphElement
     },
 }
